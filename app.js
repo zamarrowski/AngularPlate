36f451f314500c63e8dd882ee1eb03469c3caad0
zamarrowski <sergiozamarro@hotmail.com>
2016-03-06 19:29:22 +0100

    Create CRUD with (Controller, Template and Service)

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ let colors = require("colors/safe");
 let FactoryWizard = require('./app/wizards/FactoryWizard.js');
 let ControllerWizard = require('./app/wizards/ControllerWizard.js');
 let CRUDServiceWizard = require('./app/wizards/CRUDServiceWizard.js');
+let CompleteCRUDWizard = require('./app/wizards/CompleteCRUDWizard.js');
 
 let schema = {
   properties: {
     create: {
       description: colors.red('\nWelcome to Angularplate:\n') + colors.green('What do you create? \n'
-      +' 1. Factory \n 2. Controller + View \n 3. CRUD Service \n 0. Exit'),
+      +' 1. Factory \n 2. Controller + View \n 3. CRUD Service \n 4. Complete CRUD (Controller + View + Service) \n 0. Exit'),
       required: true
     }
   }
@@ -29,6 +30,9 @@ prompt.get(schema, function (err, result) {
   }
   else if (result.create == 3) {
     CRUDServiceWizard.init();
+  }
+  else if (result.create == 4) {
+    CompleteCRUDWizard.init();
   }
   else if (result.create == 0) {
     console.log('Bye!');
---
+++ app/wizards/CompleteCRUDWizard.js
@@ -0,0 +1,54 @@
+'use strict';
+let prompt = require('prompt');
+let colors = require('colors');
+let ControllerBuilder = require('./../builders/ControllerBuilder');
+let WizardParser = require('./../common/WizardParser.js');
+let ViewBuilder = require('./../builders/ViewBuilder.js');
+let ServiceBuilder = require('./../builders/ServiceBuilder');
+let JavaScriptGenerator = require('./../common/JavaScriptGenerator.js');
+
+class CompleteCRUDWizard {
+  constructor() {}
+
+  init() {
+    let schema = {
+      properties: {
+        entityName: {
+          description: colors.green('Enter entity name'),
+          message: colors.red('Name of entity is required'),
+          required: true
+        },
+        properties: {
+          description: colors.green('Write properties for your controller separated by # (name:type:value) \n')
+        },
+        dependencies: {
+          description: colors.green('Write dependencies for your controller separated by , : ($scope already added) \n')
+        },
+        frameworkStyle: {
+          description: colors.green('Framework style: \n 1. None 2. Bootstrap 3. Angular Material')
+        },
+        urlService: {
+          description: colors.green('API URL (example: http://localhost/api/entity)'),
+          required: true,
+          message: colors.red('Url name is required')
+        }
+      }
+    };
+    prompt.start();
+    prompt.get(schema, (err, result) => {
+      if (err) console.log(err);
+      let properties = WizardParser.getProperties(result.properties);
+      let dependencies = WizardParser.getDependencies(result.dependencies);
+      let serviceName = JavaScriptGenerator.getPascalCamelCaseName(result.entityName) + 'Services';
+      dependencies.push(serviceName);
+      let controller = new ControllerBuilder(`${result.entityName}Ctrl`, properties, dependencies);
+      controller.createTemplate();
+      let view = new ViewBuilder(`${result.entityName}View`, properties, result.frameworkStyle);
+      view.createTemplate();
+      let service = new ServiceBuilder(result.entityName, result.urlService, result.crudType);
+      service.createTemplate();
+    });
+  }
+}
+
+module.exports = new CompleteCRUDWizard();
